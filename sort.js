ebedfa4a6eb201929629d57f504991566e1c1516
Charish128 <charis12876@gmail.com>
2024-02-10 21:36:29 +0700

    bugfix

--- sort.js
+++ sort.js
@@ -1,52 +1,34 @@
 var basket = [10, 20, 50, 40, 25, 10];
 var list = [30, "sepuluh", 10, 233, 4285, "kosong"];
-var secondList = [
-  {},
-  10,
-  ["adsa", 3249, "0", -1],
-  1341,
-  NaN,
-  0,
-  "2",
-  false,
-  -2123,
-  "-23",
-  34,
-  "awsd",
-];
+var secondList = [ {}, 10, ["adsa", 3249, "0", -1], 1341, NaN, 0, "2", false, -2123, "-23", 34, "awsd"];
 
 function filterObject(array) {
   let number = [];
 
   for (let i = 0; i < array.length; i++) {
     if (typeof array[i] == "object") {
-      let x = array[i];
-      number.push(x);
+      if (array[i].length > 0) {
+        for (let j = 0; j < array[i].length; j++) {
+          number.push(array[i][j]);
+        }
+      }
     }
   }
 
-  let temp = number.flat();
   let object = [];
   let final = [];
 
-  for (let i = 0; i < temp.length; i++) {
-    let x = parseInt(temp[i]);
+  for (let i = 0; i < number.length; i++) {
+    let x = parseInt(number[i]);
     object.push(x);
   }
 
-  for (let i = 0; i < object.length; i++){
+  for (let i = 0; i < object.length; i++) {
     let x = object[i];
-    if(!isNaN(x)){
+    if (!isNaN(x)) {
       final.push(x);
     }
   }
-
-  // for (let i = 0; i > object.length; i++) {
-  //   let x = object[i];
-  //   if (!isNaN(x)) {
-  //     final.push(x);
-  //   }
-  // }
 
   return final;
 }
