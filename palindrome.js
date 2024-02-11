584158ee191cb8e8a2db43bc8229a0f6e46f7282
Fathur <fathur@Fathurs-MacBook-Air.local>
2024-02-11 09:05:20 +0700

    task: palindrome

---
+++ palindrome.js
@@ -0,0 +1,10 @@
+var palindrome = ["ada", "lala", "racecar" ,"turbo", "civic", "jojo" ]
+
+
+function palindromeChecker (listString) {
+    // ur code
+}
+
+console.log(palindromeChecker(palindrome))
+// output
+// ['ada', 'racecar', 'civic' ]
