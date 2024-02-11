ae2da203db8e8d3e82292377bffe99276fbd0591
Charish128 <charis12876@gmail.com>
2024-02-11 19:49:01 +0700

    practice: 110224

--- palindrome.js
+++ palindrome.js
@@ -1,10 +1,22 @@
-var palindrome = ["ada", "lala", "racecar" ,"turbo", "civic", "jojo" ]
+var palindrome = ["ada", "lala", "racecar", "turbo", "civic", "jojo"];
 
-
-function palindromeChecker (listString) {
-    // ur code
+function palindromeChecker(listString) {
+  // ma code
+  let result = [];
+  for (let i = 0; i < listString.length; i++) {
+    let x;
+    let y;
+    if (listString[i].length > 0) {
+      let info = listString[i].length;
+      x = listString[i].charAt(0);
+      y = listString[i].charAt(info - 1);
+      if (x == y) { 
+        result.push(listString[i]);
+      }
+    }
+  }
+  return result;
 }
 
-console.log(palindromeChecker(palindrome))
-// output
-// ['ada', 'racecar', 'civic']
+console.log(palindromeChecker(palindrome));
+
